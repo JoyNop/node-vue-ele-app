9e586f6127d10e4df273164cc9dfee819a9740f1
Renny <xhnr@vip.qq.com>
2018-10-11 23:40:20 +0800

    1、增加注册页面和404页面 1、增加相关页面资源

---
+++ client/src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="NotFound">
+        <img src="../assets/404.gif" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "404"
+    }
+</script>
+
+<style scoped>
+    .NotFound {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    .NotFound img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
