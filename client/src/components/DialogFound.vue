644b9c9dfcb331ce27c819407f08dbdf00b28f4a
Renny <xhnr@vip.qq.com>
2018-10-14 22:30:29 +0800

    增加弹窗样式

--- client/src/components/DialogFound.vue
+++ client/src/components/DialogFound.vue
@@ -2,12 +2,51 @@
     <!-- Form -->
     <div class="dialog">
         <el-dialog title="添加资金信息"
-                   :visible="dialog.show"
+                   :visible.sync="dialog.show"
                    :close-on-click-modal="false"
                    :close-on-press-escape="false"
                    :modal-append-to-body="false"
         >
-            
+            <div class="form">
+                <el-form
+                        ref="form"
+                        :model="formData"
+                        :rules="form_rules"
+                        label-width="120px"
+                        style="margin:10px;width: auto;"
+                >
+                    <el-form-item label="收支类型">
+                        <el-select v-model="formData.type" placeholder="收支类型">
+                            <el-option v-for="(formtype,index) in format_type_list" :key="index"
+                                       :label="formtype" :value="formtype"></el-option>
+                        </el-select>
+
+                    </el-form-item>
+                    <el-form-item prop="describe" label="收支描述:">
+                        <el-input type="describe" v-model="formData.describe"></el-input>
+                    </el-form-item>
+
+                    <el-form-item prop='income' label="收入:">
+                        <el-input type="income" v-model="formData.income"></el-input>
+                    </el-form-item>
+
+                    <el-form-item prop='expend' label="支出:">
+                        <el-input type="expend" v-model="formData.expend"></el-input>
+                    </el-form-item>
+
+                    <el-form-item prop='cash' label="账户现金:">
+                        <el-input type="cash" v-model="formData.cash"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="备注:">
+                        <el-input type="textarea" v-model="formData.remark"></el-input>
+                    </el-form-item>
+                    <el-form-item class="text-right">
+                        <el-button @click="dialog.show=false">取消</el-button>
+                        <el-button type="primary" @click="onSumbit('form')">提交</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
         </el-dialog>
     </div>
 </template>
@@ -17,6 +56,23 @@
         name: "DialogFound",
         props: {
             dialog: Object
+        },
+        data() {
+            return {
+                formData: {
+                    type: '',
+                    describe: '',
+                    income: '',
+                    expend: '',
+                    cash: '',
+                    remark: '',
+                    id: ''
+                },
+                format_type_list: [
+                    "提现",
+                    "充值"
+                ]
+            }
         }
     };
 </script>
