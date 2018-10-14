70aef686adcead4983374edaa81c9ee74bda76e5
Renny <xhnr@vip.qq.com>
2018-10-14 12:45:48 +0800

    HeadNav.vue样式修改

---
+++ client/src/views/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="index">
+        <HeadNav></HeadNav>
+    </div>
+</template>
+
+<script>
+    import HeadNav from '../components/HeadNav';
+
+    export default {
+        name: "index",
+        components: {HeadNav}
+    };
+</script>
+
+<style scoped>
+    .index {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    .rightContainer {
+        position: relative;
+        top: 0;
+        left: 180px;
+        width: calc(100% - 180px);
+        height: calc(100% - 71px);
+        overflow: auto;
+    }
+</style>
