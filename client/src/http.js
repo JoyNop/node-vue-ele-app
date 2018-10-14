b3e66e3d0d34f9f4ae670d10d9478ce89717934e
Renny <xhnr@vip.qq.com>
2018-10-14 09:05:49 +0800

    token过期处理

--- client/src/http.js
+++ client/src/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import {Message, Loading} from 'element-ui';
-import router from './router'
+import router from './router';
 
 let loading        //定义loading变量
 
@@ -16,31 +16,32 @@ function endLoading() {    //使用Element loading-close 方法
     loading.close()
 }
 
-// 请求拦截  设置统一header
+// 请求拦截
 axios.interceptors.request.use(config => {
     // 加载
-    startLoading()
-    if (localStorage.eleToken)
-        config.headers.Authorization = localStorage.eleToken
+    startLoading();
+    if (localStorage.eleToken) {
+        config.headers.Authorization = localStorage.eleToken;
+    }
     return config
 }, error => {
-    return Promise.reject(error)
-})
+    return Promise.reject(error);
+});
 
 // 响应拦截  401 token过期处理
 axios.interceptors.response.use(response => {
-    endLoading()
+    endLoading();
     return response
 }, error => {
     // 错误提醒
-    endLoading()
-    Message.error(error.response.data)
+    endLoading();
+    Message.error(error.response.data);
 
-    const {status} = error.response
-    if (status == 401) {
-        Message.error('token值无效，请重新登录')
+    const {status} = error.response;
+    if (status === 401) {
+        Message.error('token值无效，请重新登录');
         // 清除token
-        localStorage.removeItem('eleToken')
+        localStorage.removeItem('eleToken');
 
         // 页面跳转
         router.push('/login')
